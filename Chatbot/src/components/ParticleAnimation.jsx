c80465e047cf8bf3af1819c36452e2a519fee5c2
Aditya Puranik <mail.adityapuranik@gmail.com>
2025-05-13 17:54:37 +0530

    React_Project_Submission_ready

---
+++ Chatbot/src/components/ParticleAnimation.jsx
@@ -0,0 +1,77 @@
+import React, { useCallback } from "react";
+import Particles from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+function ParticleAnimation() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        fullScreen: false, // very important to keep it inside contentdiv
+        background: {
+          color: "transparent",
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.3,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            outModes: {
+              default: "bounce",
+            },
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.4,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 3 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
+
+export default ParticleAnimation;
--- Chatbot/src/screens/ContactUs.jsx
+++ Chatbot/src/screens/ContactUs.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import ContactForm from "../components/Contact";
 import Footer from "../components/Footer";
 import "../styles/ContactUs.css";
+import ParticlesAnimation from "../components/ParticleAnimation";
 
 function ContactUs() {
   return (
